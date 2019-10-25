e5a3de5931c2d463084141a8c7041e0e044e1a09
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2019-10-25 17:00:31 +0800

    5PM

---
+++ warehouseINVENTORY/Login/Forms/LoginFrm.vb
@@ -0,0 +1,26 @@
+﻿Imports MetroFramework
+Public Class LoginFrm
+    Private Sub LoginFrm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Tbox_Enter(sender As Object, e As EventArgs) Handles UserNameTbox.Enter, PasswordTbox.Enter
+        If sender Is PasswordTbox Then
+            PasswordLbl.FontWeight = MetroLabelWeight.Bold
+        ElseIf sender Is UserNameTbox Then
+            UserNameLbl.FontWeight = MetroLabelWeight.Bold
+        End If
+    End Sub
+
+    Private Sub Tbox_Leave(sender As Object, e As EventArgs) Handles UserNameTbox.Leave, PasswordTbox.Leave
+        If sender Is PasswordTbox Then
+            PasswordLbl.FontWeight = MetroLabelWeight.Light
+        ElseIf sender Is UserNameTbox Then
+            UserNameLbl.FontWeight = MetroLabelWeight.Light
+        End If
+    End Sub
+
+    Private Sub LoginBtn_Click(sender As Object, e As EventArgs) Handles LoginBtn.Click
+        LoginBtn.SendToBack()
+    End Sub
+End Class
---
+++ warehouseINVENTORY/Login/Modules/LoginMod.vb
@@ -0,0 +1,9 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Data.SqlClient
+Imports System.Security.Cryptography
+Module LoginMod
+    Public KMDISystemsConnectionType As String = Nothing
+    Public sqlconnString As String = "Data Source='192.168.1.21,49107';Network Library=DBMSSOCN;Initial Catalog='warehousedb';User ID='kmdiadmin';Password='kmdiadmin';"
+
+End Module
